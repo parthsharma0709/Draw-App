8f7e101dd022b93406e0746afe83908977d6342f
Parth Sharma <05sharmaparth@gmail.com>
2025-04-02 18:31:48 +0530

    more shapes added

--- apps/excalidraw-frontend/app/draw/index.ts
+++ apps/excalidraw-frontend/app/draw/index.ts
@@ -1,4 +1,20 @@
+
+type Shape={
+   type:"rect",
+   x:number,
+   y:number,
+   height:number,
+   width:number
+} | {
+    type:"circle",
+    centerX:number,
+    centerY:number,
+    radius:number
+}
+
+
 export function initDraw(canvas:HTMLCanvasElement){
+    const existingShapes:Shape[]=[];
     const ctx= canvas.getContext("2d");
     if(!ctx){
         return;
@@ -19,21 +35,40 @@ ctx.fillRect(0,0,canvas.width,canvas.height);
     })
     canvas.addEventListener("mouseup",(e)=>{
         clicked=false;
-        console.log(e.clientX);
-        console.log(e.clientY)
+        const height= e.clientY-startY;
+        const width= e.clientX-startX;
+        existingShapes.push({
+            type:"rect",
+            x:startX,
+            y:startY,
+            width,
+            height
+        })
     })
 
     canvas.addEventListener("mousemove",(e)=>{
         if(clicked){
             const height= e.clientY-startY;
             const width= e.clientX-startX;
-            ctx.clearRect(0,0,canvas.width,canvas.height);
-            ctx.fillStyle = "rgba(0, 0, 0)"; 
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
+           
+            clearCanvas(existingShapes,canvas,ctx);
             ctx.strokeStyle= "rgba(255,255,255)";
             ctx.strokeRect(startX,startY,width,height)
 
         }
      
     })
+}
+
+
+function clearCanvas(existingShapes:Shape[],canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D){
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+            ctx.fillStyle = "rgba(0, 0, 0)"; 
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            existingShapes.map((shape)=>{
+                if(shape.type==="rect"){
+                    ctx.strokeStyle="rgba(255,255,255)";
+                    ctx.strokeRect(shape.x,shape.y,shape.width,shape.height)
+                }
+            })
 }
