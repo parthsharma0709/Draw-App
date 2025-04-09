6087d902a8deeae085430fb4ee37efecf10ed5bb
Parth Sharma <05sharmaparth@gmail.com>
2025-04-09 16:32:22 +0530

    some UI changes added

--- packages/ui/src/input-box.tsx
+++ packages/ui/src/input-box.tsx
@@ -1,16 +1,20 @@
 "use client"
+import { forwardRef } from 'react';
+import { ForwardedRef } from 'react';
 
 interface InputProps{
     label:string,
     type:string,
     placeholder:string,
-    classname?:string
+    classname?:string,
+  
 
 }
 
-export const Input=({label,type,placeholder,classname=""}:InputProps)=> {
+export const Input=forwardRef(({label,type,placeholder,classname=""}:InputProps, ref:ForwardedRef<HTMLInputElement>)=> {
     return <div className="flex flex-col gap-2">
     <h1>{label}</h1>
-    <input type= {type} placeholder={placeholder} className={classname}/>
+    <input ref={ref} type= {type} placeholder={placeholder} className={classname}/>
     </div>
-}
+})
+Input.displayName = 'Input';
