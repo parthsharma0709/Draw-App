9c0b011c2a0a11a5bf1475dceecea310741b770e
Parth Sharma <05sharmaparth@gmail.com>
2025-04-11 15:48:26 +0530

    circle drawing added

--- apps/excalidraw-frontend/app/components/canvas.tsx
+++ apps/excalidraw-frontend/app/components/canvas.tsx
@@ -1,14 +1,19 @@
 "use client"
-import { useEffect, useRef} from "react"
+import { useEffect, useRef, useState} from "react"
 import { initDraw } from "../draw";
 
 import {Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
 import { IconButton } from "./icons";
 
+type Shape= "circle" | "rect"| "pencil" ;
 
 export default function Canvas({roomId,socket}:{roomId:string,socket:WebSocket}){
     const canvasRef= useRef<HTMLCanvasElement>(null);
-  
+  const [selectedTool, setSelectedTool]= useState<Shape>("circle");
+  useEffect(()=>{
+ //@ts-ignore
+    window.selectedTool= selectedTool
+  },[selectedTool])
 
   useEffect(()=>{
     if(canvasRef.current){
@@ -24,17 +29,17 @@ export default function Canvas({roomId,socket}:{roomId:string,socket:WebSocket})
     return <div className="overflow-hidden static h-100vh">
     
    <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight} ></canvas>
- <TopBar/>
+ <TopBar setSelectedTool={setSelectedTool} selectedTool= {selectedTool} />
     </div>
 }
 
-function TopBar(){
+function TopBar({selectedTool,setSelectedTool}:{selectedTool:Shape,setSelectedTool:(s:Shape)=>void}){
   return (
     <div className="absolute top-0 left-0 p-3 w-full px-8 py-3 flex justify-evenly items-center bg-slate-400 shadow-md">
     
-    <IconButton icon={<Pencil/>} onClick={()=>{}}/>
-    <IconButton icon={<RectangleHorizontalIcon/>} onClick={()=>{}}/>
-    <IconButton icon={<Circle/>} onClick={()=>{}}/>
+    <IconButton activated= {selectedTool==="pencil"}  icon={<Pencil/>} onClick={()=>{setSelectedTool("pencil")}}/>
+    <IconButton activated={selectedTool==="rect"} icon={<RectangleHorizontalIcon/>} onClick={()=>{setSelectedTool("rect")}}/>
+    <IconButton activated={selectedTool==="circle"} icon={<Circle/>} onClick={()=>{setSelectedTool("circle")}}/>
     
     
     </div>
--- apps/excalidraw-frontend/app/components/icons.tsx
+++ apps/excalidraw-frontend/app/components/icons.tsx
@@ -1,8 +1,8 @@
 import { LineChart } from "lucide-react";
 import { ReactNode } from "react";
 
-export function IconButton({icon,onClick}:{icon:ReactNode, onClick: ()=>void}){
-    return <div className=" pointer rounded-full border p-2 " onClick={onClick}>
+export function IconButton({icon,onClick,activated}:{icon:ReactNode, onClick: ()=>void, activated:boolean}){
+    return <div className= {`pointer rounded-full border p-2  ${activated ? "text-red-800":"text-black"}`} onClick={onClick}>
         {icon}
     </div>
 }
