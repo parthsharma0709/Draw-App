f499c3f940a95cd0a2cffa2a2e9844f8a7ffe0d6
Parth Sharma <05sharmaparth@gmail.com>
2025-04-13 11:26:23 +0530

    headers added

--- apps/excalidraw-frontend/app/draw/getshapes.ts
+++ apps/excalidraw-frontend/app/draw/getshapes.ts
@@ -1,6 +1,12 @@
 import axios from "axios";
 export async function getExistingShapes(roomId: string) {
-    const response = await axios.get(`http://localhost:3001/api/v1/user/chats/${roomId}`);
+    const response = await axios.get(`http://localhost:3001/api/v1/user/chats/${roomId}`,
+        {
+            headers: {
+              Authorization: localStorage.getItem("token"),
+            },
+          }
+    );
     if (!response.data) return [];
 
     const messages = response.data.texts;
